004a0ae1e2d8c29807209ce3e148b13558619b9b
Charly Gomez <charly.gomez1310@gmail.com>
2022-09-22 15:56:54 +0200

    chore: add daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,19 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#0072bc',
+          'primary-focus': '#006496',
+          secondary: '#fcb913',
+          accent: '#f96400',
+        },
+      },
+    ],
+  },
 };
