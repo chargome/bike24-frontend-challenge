36b6e958a03b4b49042c162394f29885f9ed039b
Charly Gomez <charly.gomez1310@gmail.com>
2022-09-24 10:57:29 +0200

    feat: closes #5, #15; display total price in form

--- src/components/AddProductForm/AddProductForm.spec.tsx
+++ src/components/AddProductForm/AddProductForm.spec.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { describe, expect, it, vi } from 'vitest';
 import * as hooks from '../../hooks';
+import { Product } from '../../model';
 import { render, screen } from '../../test/test-utils';
 import { AddProductForm } from './AddProductForm';
 describe('AddProductForm', () => {
@@ -20,5 +22,24 @@ describe('AddProductForm', () => {
   it('should display add button', () => {
     render(<AddProductForm />);
     expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('should not display total when no product is selected', () => {
+    render(<AddProductForm />);
+    expect(screen.queryByText(/Total:/i)).toBeNull();
+  });
+
+  it('should display total when product is selected', () => {
+    const mockProduct: Partial<Product> = {
+      price: 123,
+      taxRate: 10,
+    };
+    const setStateMock = vi.fn();
+    vi.spyOn(React, 'useState').mockImplementation(() => [
+      mockProduct,
+      setStateMock,
+    ]);
+    render(<AddProductForm />);
+    expect(screen.getByText(/Total:/i)).toBeInTheDocument();
   });
 });
